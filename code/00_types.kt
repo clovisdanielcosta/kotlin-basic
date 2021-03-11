d85696db3a75f61bf2b9a87b533f7c39afc4b0fc
clovisdanielcosta <clovisdanielcosta@hotmail.com>
2021-03-11 13:08:31 -0300

    Added files types and var_val_const_val

---
+++ code/00_types.kt
@@ -0,0 +1,5 @@
+//Int, Long, Float, Double, Array, Boolean, Char, Byte, Short
+// Null
+
+println(Int.MAX_VALUE)
+println(Double.MIN_VALUE)
---
+++ code/01_var_val_const_val.kt
@@ -0,0 +1,12 @@
+// Var
+var text: String = "Hello World!"
+
+// Val (Imutável após a atribuição em tempo de execução.)
+val firstText: String = "Say: "
+
+// Const Val (Precisa ser declarado fora do bloco. Imutável desde a compilação)
+const val TO_WHOM: String = "Everybody!"
+
+// Inferência
+var text2 = "Say it again"
+
